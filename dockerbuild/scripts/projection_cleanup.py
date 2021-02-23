c7a5a90651b9ff31703f7549990a0dca5cf01164
Loc Huynh <40050301+Locne55@users.noreply.github.com>
2021-02-23 13:47:49 -0600

    bulk of project

--- dockerbuild/scripts/projection_cleanup.py
+++ dockerbuild/scripts/projection_cleanup.py
@@ -6,6 +6,7 @@ import xmltodict
 
 #create engine for reading postgres tables into dataframes
 engine = sql.create_engine('postgresql+psycopg2://postgres:123@db:5432/postgres')
+#bad practice to store password in plain text should use a secrets file but time did not permit
 
 #read projections table into df1
 df1 = pd.read_sql(sql='select * from projections', con=engine)
@@ -29,3 +30,17 @@ finaldf.to_sql('projections_cleaned', engine, index_label = 'projectionid_cleane
     'MonthYear': psql.TIMESTAMP,
     'Payment': psql.MONEY},
                if_exists='replace')
+#label projectionslabel by creating column that increments every 12 rows
+df12period = pd.read_sql(sql="select * from projections_cleaned where \"Description\"<>'Starting Balance'", con=engine)
+df12period['Projection']=(df12period.index / 12 + 1).astype(int)
+df12period.to_sql('projections_cleaned', engine, dtype={
+    'projectionid_cleaned':psql.INTEGER,
+    'Description':psql.VARCHAR,
+    'Projected': psql.MONEY,
+    'Deposit' : psql.MONEY,
+    'MonthYear': psql.TIMESTAMP,
+    'Payment': psql.MONEY,
+    'Projections': psql.INTEGER},
+               if_exists='replace')
+
+#turn this script into a function for re-usability, time did not permit
